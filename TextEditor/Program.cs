30c8c7dfe4291f6402070a705cf2ad26e2fcf645
Camila <caamiladepaula03@gmail.com>
2024-08-18 08:08:29 -0300

    atualizando

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("O que deseja fazer?");
+      Console.WriteLine("1- Abrir arquivo");
+      Console.WriteLine("2- Criar novo arquivo");
+      Console.WriteLine("\n0 - Sair");
+      Console.WriteLine("-------------------");
+      short opcao = short.Parse(Console.ReadLine());
+
+      switch (opcao)
+      {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Editar(); break;
+        default: Menu(); break;
+      }
+    }
+
+    static void Abrir()
+    {
+      Console.Clear();
+      Console.WriteLine("Qual o caminho do arquivo?");
+      string caminho = Console.ReadLine();
+
+      using (var arquivo = new StreamReader(caminho))
+      {
+        string texto = arquivo.ReadToEnd();
+        Console.WriteLine(texto);
+      }
+      Console.WriteLine("");
+      Console.ReadLine();
+      Menu();
+    }
+
+    static void Editar()
+    {
+      Console.Clear();
+      Console.WriteLine("Editor de texto (ESC para sair)");
+      Console.WriteLine("------------------------");
+      string texto = "";
+
+      do
+      {
+        texto += Console.ReadLine();
+        texto += Environment.NewLine;
+      }
+      while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+      Salvar(texto);
+    }
+
+    static void Salvar(string texto)
+    {
+      Console.Clear();
+      Console.WriteLine("Qual caminho para salvar o arquivo?");
+      var caminho = Console.ReadLine();
+
+      using (var arquivo = new StreamWriter(caminho))
+      {
+        arquivo.Write(texto);
+      }
+
+      Console.WriteLine($"Arquivo {caminho} salvo com sucesso!");
+      Console.ReadLine();
+
+      Menu();
+    }
+  }
+}
