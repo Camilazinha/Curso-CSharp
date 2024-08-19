c0e21d93713afc649a3ea3ba8c3df8e3950850df
Camila <caamiladepaula03@gmail.com>
2024-08-18 23:35:23 -0300

    correcao-do-erro

--- EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7b03f331777eecb9a4156ca1bd5fb7be98df1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee52c040d8b518b67586961f827e1403b8d2401")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
